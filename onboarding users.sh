dcb90f67e0f3b9fa46ec6ed19cb231b146dcf3dc
Osazee Odigie <osazee.odigie01@gmail.com>
2023-03-03 18:24:32 +0100

    Fix the onboarding users script

--- onboarding users.sh
+++ onboarding users.sh
@@ -1,5 +1,16 @@
-user1
-user2
-USER3
-useR4
-User5
+#! /bin/bash
+
+usergroup='developers'
+while IFS=, read -r line;
+do 
+    echo "${line,,}";
+    if id "${line,,}" &>/dev/null; then
+        echo 'user already exist'
+    else
+        sudo useradd -s /bin/bash -d "/home/${line,,}" -m -G ${usergroup} ${line,,}
+        sudo mkdir /home/${line,,}/.ssh
+        sudo chmod 0700 /home/${line,,}/.ssh
+        sudo -- sh -c "cat /home/$USER/.ssh/id_rsa.pub > /home/${line,,}/.ssh/authorized_keys"
+        sudo chown -R ${line,,}:${line,,} /home/${line,,}/.ssh
+    fi
+done < names.csv
